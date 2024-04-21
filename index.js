40ae73a3e53aca23897b098910a1e8a23915610f
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-04-21 23:00:26 +0200

    code for generator added

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import fs from "fs";
 import inquirer from "inquirer";
-import qrimage from "qr-image";
+import qr from "qr-image";
 
-fs.writeFileSync("UserInput.txt", "")
+
+inquirer
+    .prompt([
+        {"message": "Type in your URL: ", name: "URL"}
+    ])
+    .then((answers) => {
+        const url = answers.URL
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream("qr_img.png"))
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+
+        } else {
+
+        }
+    });
